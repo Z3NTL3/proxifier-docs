f487a9b1d2d15b32126b9ab44eed0b141fb73247
z3ntl3 <z3ntl3discord@gmail.com>
2025-09-17 16:26:48 +0200

    09/17/2025 16:26:48

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,9 @@ import Header from '../components/Header';
 import Head from '@docusaurus/Head';
 import Intro from '../components/Intro';
 import Feats from '../components/Feats';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import {MorphSVGPlugin} from 'gsap/all';
 
 const tailwindTheme = `
 @import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
@@ -22,6 +25,9 @@ const tailwindTheme = `
   }
 }
 `
+
+gsap.registerPlugin(useGSAP)
+gsap.registerPlugin(MorphSVGPlugin)
 
 const HomeMeta = () => (
   <Head>
